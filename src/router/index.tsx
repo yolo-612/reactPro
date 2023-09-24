f7d7550f456080c8cb302463fab47b1fd9413d4e
keruisiya <2458097476@qq.com>
2023-09-24 20:39:02 +0800

    路由懒加载的配置

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,13 @@
-
+import React, { lazy } from "react"
 import Home from "../views/Home"
-import About from "../views/About"
+// import About from "../views/About"
+
+const About = lazy(()=>import("../views/About"))
+
+// 懒加载报错：Uncaught Error: A component suspended while responding to synchronous input. This will cause the UI to be replaced with a loading indicator.
+//  To fix, updates that suspend should be wrapped with startTransition.
+// 懒加载的模式 需要外面结合suspend一起使用
+
 import {Navigate} from "react-router-dom"
 
 const routes = [
@@ -14,7 +21,9 @@ const routes = [
   },
   {
     path: "/about",
-    element: <About></About>
+    element: <React.Suspense fallback={<div>Loading</div>}>
+      <About></About>
+    </React.Suspense> 
   }
 ]
 
