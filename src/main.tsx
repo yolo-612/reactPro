e0f9b9380fd514abd6a02607a98fe43467e495f0
keruisiya <2458097476@qq.com>
2023-09-24 17:09:47 +0800

    05+06样式初始化+sass配置

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// 样式初始化一般放在最前面放在app的上面
+import "reset-css"
+// UI框架的样式
+
+// 全局样式【要去覆盖ui框架的样式的】
+import "../src/assets/styles/global.scss"
+
+// 组件的样式
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
