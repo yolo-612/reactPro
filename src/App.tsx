b41812ae0969fa4701ca633a1c891b129be650a1
keruisiya <2458097476@qq.com>
2023-09-24 20:26:12 +0800

    路由的标准hook的写法

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
-import { Outlet, Link } from "react-router-dom"
+// import { Outlet, Link } from "react-router-dom"
+import { useRoutes, Link } from "react-router-dom" // 使用内置的创建对象的路由的hook
+
+import router from "./router"
 
 function App() {
   const [count, setCount] = useState(0)
+  const outlet = useRoutes(router)
 
   return (
     <div className='App'>
-      {/* 占位符组件，类似于窗口，用来展示组件，有点像vue中的router-view */}
       <Link to="/home">Home</Link>
       <Link to="/about">About</Link>
-      <Outlet></Outlet>
+      {/* 占位符组件，类似于窗口，用来展示组件，有点像vue中的router-view */}
+      {/* <Outlet></Outlet> */}
+
+      {outlet}
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,15 @@ import "reset-css"
 import "@/assets/styles/global.scss"
 
 // 组件的样式
-// import App from './App.tsx'
-import Router from './router'
+
+import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+// import Router from './router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
